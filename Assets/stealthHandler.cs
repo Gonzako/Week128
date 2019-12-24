2e60bea3764621656f3868e470be9ce3d9036fdd
Gonzako <al341522@uji.es>
2019-12-24 17:16:24 +0100

    created idle animation + stealth basic anim support

--- Assets/stealthHandler.cs
+++ Assets/stealthHandler.cs
@@ -4,36 +4,65 @@
  * Gonzako123@gmail.com
  *
  */
- 
+
+using System;
 using UnityEngine;
- 
+
+
+/// <summary>
+/// Manages animation and stealth state
+/// </summary>
 public class stealthHandler : MonoBehaviour
 {
     #region Public Fields
     #endregion
- 
+
     #region Private Fields
+    characterMovement charMov;
+    Animator animator;
     #endregion
 
     #region Public Methods
     #endregion
 
     #region Private Methods
+    private void camoStopHandle()
+    {
+        animator.SetBool("Stealthing", false);
+    }
+
+    private void camoStartHandle()
+    {
+        animator.SetBool("Stealthing", true);
+    }
     #endregion
 
 
-    #if true
+#if true
     #region Unity API
 
-    void Start()
+    void Awake()
     {
+        animator = GetComponent<Animator>();
+        charMov = GetComponent<characterMovement>();
+        charMov.onCamoStart += camoStartHandle;
+        charMov.onCamoFinish += camoStopHandle;
     }
- 
+
+
+
+    private void OnDisable()
+    {
+        charMov.onCamoStart -= camoStartHandle;
+        charMov.onCamoFinish -= camoStopHandle;
+    }
     void FixedUpdate()
     {
     }
 
+
+
     #endregion
-    #endif
- 
+#endif
+
 }
